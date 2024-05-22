f204961e5ae5471495b1555a2e55e906528d9ff4
Huỳnh Phương Đại <phuongdai.saigon@gmail.com>
2024-05-22 14:25:07 +0700

    gamestack staging

--- src/gamestack.java
+++ src/gamestack.java
@@ -1,3 +1,44 @@
+import java.util.Stack;
 public class gamestack {
-    
+    private char[][] board;
+    private boolean[][] revealed;
+    private boolean[][] mines;
+    private Integer numRevealed;
+
+    private Stack<char[][]> boardStack;
+    private Stack<boolean[][]> revealedStack;
+    private Stack<boolean[][]> minesStack;
+    private Stack<Integer> numRevealedStack;
+
+    public GameStack(char[][] board, boolean[][] revealed, boolean[][] mines) {
+        
+    }
+
+    public char[][] getBoard() {
+        return board;
+    }
+
+    public boolean[][] getRevealed() {
+        return revealed;
+    }
+
+    public boolean[][] getMines() {
+        return mines;
+    }
+
+    public int getnumRevealed() {
+        return numRevealed;
+    }
+
+    public boolean canUndo() {
+        return !boardStack.isEmpty();
+    }
+
+    public void saveSnapshot() {
+        
+    }
+
+    public void undo() {
+        
+    }
 }
