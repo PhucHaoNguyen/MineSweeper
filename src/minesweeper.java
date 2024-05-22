18189814bc10d9eca96cab2ca29635db0427b21e
Huỳnh Phương Đại <phuongdai.saigon@gmail.com>
2024-05-22 14:23:04 +0700

    game and board initialization

--- src/minesweeper.java
+++ src/minesweeper.java
@@ -1,5 +1,4 @@
 import java.util.Random;
-
 public class minesweeper{
     private Random random;
     
@@ -16,11 +15,33 @@ public class minesweeper{
     public boolean[][] revealed;
 
     public minesweeper(){
-
+        board = new char[10][10];
+        revealed = new boolean[10][10];
+        mines = new boolean[10][10];
+        random = new Random();
+    //board size is 10x10 set-in-stone for now
     }
 
     public void initializeBoard(){
+        for (int row = 0; row < 10; row++) {
+            for (int col = 0; col < 10; col++) {
+                board[row][col] = UNREVEALED;
+                revealed[row][col] = false;
+                mines[row][col] = false;
+            }
+        }
 
+        int numAdded = 0;
+        while (numAdded < numMines) {
+            int row = random.nextInt(10);
+            int col = random.nextInt(10);
+
+            if (!mines[row][col]) {
+                mines[row][col] = true;
+                numAdded++;
+            }
+        }
+        GameStack = new GameStack(board, revealed, mines);
     }
 
     public void revealCell(){
